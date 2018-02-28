06ed22f6f3ae0cb4c6cf544ebc507b5e21caf260
Maycon <mayconn.aguiarr@gmail.com>
2018-02-27 21:19:00 -0300

    Alterado 'import.sql' e deletado pasta de test

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
-insert into Book (author, title, category) values ('Oliveira Lima', ' O Movimento da Independência - 1821- 1822', '5' );
-insert into Book (author, title, category) values ('Olavo Bilac', ' Melodrama em Santo Tirso', '1' );
-insert into Book (author, title, category) values ('Raymundo Magalhães', ' Memórias de Um Sargento de Milícias ', '3' );
-insert into Book (author, title, category) values ('Joana Quill', ' O Lobisomem  - 1821- 1822', '1' );
-insert into Book (author, title, category) values ('Thomaz Lopes ', ' Memórias de outra Mulher', '2' );
-insert into Book (author, title, category) values ('Urbano Duarte', ' O Defunto ', '1' );
-insert into Book (author, title, category) values ('Vicente de Carvalho', '    Memórias duma Mulher da Época', '4' );
+insert into Book (author, title, category) values ('Oliveira Lima', ' O Movimento da Independência - 1821- 1822', 'Revolução' );
+insert into Book (author, title, category) values ('Olavo Bilac', ' Melodrama em Santo Tirso', 'Categoria 2' );
+insert into Book (author, title, category) values ('Raymundo Magalhães', ' Memórias de Um Sargento de Milícias ', 'Categoria 4' );
+insert into Book (author, title, category) values ('Joana Quill', ' O Lobisomem  - 1821- 1822', 'Terror' );
+insert into Book (author, title, category) values ('Thomaz Lopes ', ' Memórias de outra Mulher', 'Drama' );
+insert into Book (author, title, category) values ('Urbano Duarte', ' O Defunto ', 'Terror' );
+insert into Book (author, title, category) values ('Vicente de Carvalho', '    Memórias duma Mulher da Época', 'Drama' );
