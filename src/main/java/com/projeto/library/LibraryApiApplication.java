283876ff246278871365076725a36c07956fb66a
Maycon <mayconn.aguiarr@gmail.com>
2018-06-04 22:58:23 -0300

    adicionado a função 'callback'

--- src/main/java/com/projeto/library/LibraryApiApplication.java
+++ src/main/java/com/projeto/library/LibraryApiApplication.java
@@ -2,6 +2,8 @@ package com.projeto.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 
 @SpringBootApplication
 public class LibraryApiApplication {
@@ -9,4 +11,11 @@ public class LibraryApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
 	}
+	
+	@ControllerAdvice
+    static class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
+        public JsonpAdvice() {
+            super("callback");
+        }
+	}		
 }
